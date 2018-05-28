241330d059f9347ea7ca0371a2c73df6610519c9
Paul Smith <code@uvwxy.de>
2018-05-28 20:36:18 +0200

    update .gitignore, add cli param for delay

--- src/main/java/de/uvwxy/mousewiggle/Main.java
+++ src/main/java/de/uvwxy/mousewiggle/Main.java
@@ -1,15 +1,20 @@
 package de.uvwxy.mousewiggle;
 
 import java.awt.*;
-import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
+        int delay = 500;
+        try {
+            delay = Integer.parseInt(args[0]);
+        } catch (Exception e) {
+            // NOP
+        }
         Robot hal = new Robot();
         boolean up = true;
         while (true) {
-            hal.delay(1000);
+            hal.delay(delay);
             Point p = MouseInfo.getPointerInfo().getLocation();
             hal.mouseMove(p.x, p.y + (up ? 1 : -1));
             up = !up;
